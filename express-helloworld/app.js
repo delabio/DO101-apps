a030da03a8fc54b7c0328c110ae5960f18fcaf91
Artur Delabio <arturdelabio@gmail.com>
2023-02-10 18:18:27 +0000

    commit

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,10 +9,6 @@ app.get('/mars', function (req, res) {
 res.send('Hello Mars!\n');
 });
 
-app.get('/mars', function(req, res) {
-  res.send('Hello Mars!\n');
-});
-
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
